ccfdf7c5757f0e508606064a3f3327139ce2e3b1
Ashish Sharda <ashishjsharda@gmail.com>
2023-10-29 06:43:10 -0400

    first commit

---
+++ src/main/java/com/example/corsdemo2/CorsDemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.corsdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorsDemo2Application.class, args);
+    }
+
+}
